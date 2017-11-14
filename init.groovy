6cebd875e5dd8b5258358107bf125354283e187c
aknysh <aknysh>
2017-11-13 21:52:13 -0500

    Add support for GitHub Authentication. Configure from Groovy script

--- init.groovy
+++ init.groovy
@@ -4,30 +4,87 @@ import hudson.security.*
 import jenkins.model.*
 import jenkins.security.s2m.AdminWhitelistRule
 
+def isValidString = { value ->
+    if (value != null && value instanceof String && value.trim() != "") {
+        return true
+    }
+    return false
+}
+
 def env = System.getenv()
+def jenkins = Jenkins.getInstance()
+jenkins.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
 
-if (!env.JENKINS_USER) {
-    throw new Throwable("`JENKINS_USER' ENV variable is required to create the initial admin user")
-}
-
-if (!env.JENKINS_PASS) {
-    throw new Throwable("`JENKINS_PASS' ENV variable required to create the initial admin user")
-}
-
+// Set number of job executors
 int num_executors = 4
 if (env.JENKINS_NUM_EXECUTORS) {
     num_executors = env.JENKINS_NUM_EXECUTORS.toInteger()
 }
+jenkins.setNumExecutors(num_executors)
 
-def jenkins = Jenkins.getInstance()
-jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
-jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
-user.save()
+def setupSecurity = { clientId, clientSecret, admins, orgNames, oauthScopes, jenkinsUser, jenkinsPassword ->
+    // Try to configure Github Authentication Security Realm and GitHub Committer Authorization Strategy
+    if (isValidString(clientId) && isValidString(clientSecret) && isValidString(admins) && isValidString(orgNames) && isValidString(oauthScopes)) {
+        def githubSecurityRealm = new org.jenkinsci.plugins.GithubSecurityRealm(
+                "https://github.com",
+                "https://api.github.com",
+                clientId,
+                clientSecret,
+                oauthScopes
+        )
 
-jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
-jenkins.save()
+        /**
+         * GithubAuthorizationStrategy constructor params
+         * source: https://github.com/jenkinsci/github-oauth-plugin/blob/master/src/main/java/org/jenkinsci/plugins/GithubAuthorizationStrategy.java
+         * ==============================================
+         * String adminUserNames,
+         * boolean authenticatedUserReadPermission,
+         * boolean useRepositoryPermissions,
+         * boolean authenticatedUserCreateJobPermission,
+         * String organizationNames,
+         * boolean allowGithubWebHookPermission,
+         * boolean allowCcTrayPermission,
+         * boolean allowAnonymousReadPermission
+         * ==============================================
+         * Please see source for latest code if this throws errors
+         **/
+        def authorizationStrategy = new org.jenkinsci.plugins.GithubAuthorizationStrategy(
+                admins,    /*adminUserNames*/
+                true,      /*authenticatedUserReadPermission*/
+                true,      /*useRepositoryPermissions*/
+                true,      /*authenticatedUserCreateJobPermission*/
+                orgNames,  /*organizationNames*/
+                true,      /*allowGithubWebHookPermission*/
+                false,     /*allowCcTrayPermission*/
+                false)     /*allowAnonymousReadPermission*/
 
-Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
-Jenkins.instance.setNumExecutors(num_executors)
+        jenkins.setSecurityRealm(githubSecurityRealm)
+        jenkins.setAuthorizationStrategy(authorizationStrategy)
+        jenkins.save()
+    } else {
+        // If any of Github Authentication settings are not provided, try to configure Matrix-based Security Realm
+        if (!isValidString(jenkinsUser)) {
+            throw new Throwable("`JENKINS_USER' ENV variable is required to create the initial admin user")
+        }
+        if (!isValidString(jenkinsPassword)) {
+            throw new Throwable("`JENKINS_PASS' ENV variable required to create the initial admin user")
+        }
+        jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+        jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+        def user = jenkins.getSecurityRealm().createAccount(jenkinsUser, jenkinsPassword)
+        user.save()
+        jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, jenkinsUser)
+        jenkins.save()
+    }
+}
+
+setupSecurity(
+        env.JENKINS_GITHUB_CLIENT_ID,
+        env.JENKINS_GITHUB_CLIENT_SECRET,
+        env.JENKINS_GITHUB_ADMINS,
+        env.JENKINS_GITHUB_ORG_NAMES,
+        env.JENKINS_GITHUB_OAUTH_SCOPES,
+        env.JENKINS_USER,
+        env.JENKINS_PASS
+)
